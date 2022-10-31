e826850451783fe1deef7bf5dd1bba5008b72fc9
Bayron Campaz <bayron@thinkbridge.com>
2022-10-31 13:54:38 -0500

    Added initial abstraction classes for the client

---
+++ api/BookingApplication/Src/Core/Domain/Abstractions/Repositories/IClientRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions.Repositories
+{
+    public interface IClientRepository
+    {
+        public Task<IEnumerable<Client>> GetAllClients();
+        public Task<Client> GetClientById(Guid id);
+        public Task<Client> CreateClient(Client client);
+        public Task<Client> UpdateClient(Client client);
+        public Task<Client> DeleteClient(Guid id);
+
+    }
+}
